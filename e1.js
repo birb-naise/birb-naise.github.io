ff12e0a816de4b44f46e180b6144867c7e9c242b
birb ♪ <birbnaise@gmail.com>
2020-02-28 02:33:36 -0800

    changes

--- e1.js
+++ e1.js
@@ -2,6 +2,7 @@ class e1 extends Phaser.Scene {
     constructor() {
         super({key: "e1"});
     }
+
     preload(){
         this.load.image('bg','assets/bg.png');
         this.load.image('char_h','assets/char_h.png');
@@ -10,11 +11,35 @@ class e1 extends Phaser.Scene {
         this.load.image('b_feed','assets/b_feed.png');
         this.load.image('b_gift','assets/b_gift.png');
         this.load.image('b_pet','assets/b_pet.png');
+        this.load.image('happy','assets/happy.png');
     }
     create(){
+        let pos = 0;
+
+        this.image = this.add.image(pos,100,'happy');
         this.image = this.add.image(375,550,'bg');
-        this.image = this.add.image(375,750,'char_m');
-        this.image = this.add.image(187,1000,'b_feed');
+        this.image = this.add.image(375,750,'char_s');
+        const feed_press = this.add.image(187,1000,'b_feed')
+                           .setInteractive()
+                           .on('pointerdown', () => { this.updateHappy(pos+=10); });
+        this.image = this.add.image(375,1000,'b_gift');
+        this.image = this.add.image(562,1000,'b_pet');
+
+        this.updateHappy(pos)
+    }
+
+    updateHappy(pos) {
+        this.image = this.add.image(pos,100,'happy');
+        this.image = this.add.image(375,550,'bg');
+        if(pos >= 175 && pos < 350) 
+            this.image = this.add.image(375,750,'char_m');
+        else if(pos >= 350)
+            this.image = this.add.image(375,750,'char_h');
+        else
+            this.image = this.add.image(375,750,'char_s');
+        const feed_press = this.add.image(187,1000,'b_feed')
+                           .setInteractive()
+                           .on('pointerdown', () => { this.updateHappy(pos+=10); });
         this.image = this.add.image(375,1000,'b_gift');
         this.image = this.add.image(562,1000,'b_pet');
     }
